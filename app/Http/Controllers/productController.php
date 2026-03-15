737696f312ce7a98afc780bf0db41d14b5531f28
mariam osomoku <b00162501@mytudublin.ie>
2026-03-15 22:06:29 +0000

    Step 1 - Shop window grid displaying products

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateproductRequest;
+use App\Http\Requests\UpdateproductRequest;
+use App\Repositories\productRepository;
+use App\Http\Controllers\AppBaseController;
+use Illuminate\Http\Request;
+use Flash;
+use Response;
+
+class productController extends AppBaseController
+{
+    /** @var productRepository $productRepository*/
+    private $productRepository;
+
+    public function __construct(productRepository $productRepo)
+    {
+        $this->productRepository = $productRepo;
+    }
+
+    public function index(Request $request)
+    {
+        $products = $this->productRepository->all();
+
+        return view('products.index')
+            ->with('products', $products);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(CreateproductRequest $request)
+    {
+        $input = $request->all();
+
+        $product = $this->productRepository->create($input);
+
+        Flash::success('Product saved successfully.');
+
+        return redirect(route('products.index'));
+    }
+
+    public function show($id)
+    {
+        $product = $this->productRepository->find($id);
+
+        if (empty($product)) {
+            Flash::error('Product not found');
+
+            return redirect(route('products.index'));
+        }
+
+        return view('products.show')->with('product', $product);
+    }
+
+    public function edit($id)
+    {
+        $product = $this->productRepository->find($id);
+
+        if (empty($product)) {
+            Flash::error('Product not found');
+
+            return redirect(route('products.index'));
+        }
+
+        return view('products.edit')->with('product', $product);
+    }
+
+    public function update($id, UpdateproductRequest $request)
+    {
+        $product = $this->productRepository->find($id);
+
+        if (empty($product)) {
+            Flash::error('Product not found');
+
+            return redirect(route('products.index'));
+        }
+
+        $product = $this->productRepository->update($request->all(), $id);
+
+        Flash::success('Product updated successfully.');
+
+        return redirect(route('products.index'));
+    }
+
+    public function destroy($id)
+    {
+        $product = $this->productRepository->find($id);
+
+        if (empty($product)) {
+            Flash::error('Product not found');
+
+            return redirect(route('products.index'));
+        }
+
+        $this->productRepository->delete($id);
+
+        Flash::success('Product deleted successfully.');
+
+        return redirect(route('products.index'));
+    }
+
+    public function displayGrid(Request $request)
+    {
+        $products = \App\Models\Product::all();
+        return view('products.displaygrid')->with('products', $products);
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-static-top">
+        <ul class="nav navbar-nav">
+            <li><a href="{{ route('products.displaygrid') }}">Shop</a></li>
+            <li><a href="{{ route('scorders.index') }}">Orders</a></li>
+        </ul>
+    </nav>
+
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8">
+                    @yield('content')
+                </div>
+                <div class="col-lg-2"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
