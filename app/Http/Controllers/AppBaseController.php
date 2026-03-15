83883b3478dc6ee7b672ce328adb1d1d4d42a131
mariam osomoku <b00162501@mytudublin.ie>
2026-03-15 22:39:28 +0000

    Step 2 - Navbar and Add to Cart working

--- app/Http/Controllers/AppBaseController.php
+++ app/Http/Controllers/AppBaseController.php
@@ -2,21 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Routing\Controller as BaseController;
 use InfyOm\Generator\Utils\ResponseUtil;
 use Response;
 
-/**
- * @SWG\Swagger(
- *   basePath="/api/v1",
- *   @SWG\Info(
- *     title="Laravel Generator APIs",
- *     version="1.0.0",
- *   )
- * )
- * This class should be parent class for other API controllers
- * Class AppBaseController
- */
-class AppBaseController extends Controller
+class AppBaseController extends BaseController
 {
     public function sendResponse($result, $message)
     {
--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateproductRequest;
 use App\Http\Requests\UpdateproductRequest;
 use App\Repositories\productRepository;
-use App\Http\Controllers\AppBaseController;
+use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Flash;
 use Response;
+use Session;
 
-class productController extends AppBaseController
+class productController extends BaseController
 {
-    /** @var productRepository $productRepository*/
     private $productRepository;
 
     public function __construct(productRepository $productRepo)
@@ -20,10 +20,33 @@ class productController extends AppBaseController
         $this->productRepository = $productRepo;
     }
 
+    public function additem($productid)
+    {
+        if (Session::has('cart')) {
+            $cart = Session::get('cart');
+            if (isset($cart[$productid])) {
+                $cart[$productid]=$cart[$productid]+1;
+            }
+            else {
+                $cart[$productid]=1;
+            }
+        }
+        else {
+            $cart[$productid]=1;
+        }
+        Session::put('cart', $cart);
+        return Response::json(['success'=>true,'total'=>array_sum($cart)],200);
+    }
+
+    public function displayGrid(Request $request)
+    {
+        $products = \App\Models\Product::all();
+        return view('products.displaygrid')->with('products', $products);
+    }
+
     public function index(Request $request)
     {
         $products = $this->productRepository->all();
-
         return view('products.index')
             ->with('products', $products);
     }
@@ -36,77 +59,52 @@ class productController extends AppBaseController
     public function store(CreateproductRequest $request)
     {
         $input = $request->all();
-
         $product = $this->productRepository->create($input);
-
         Flash::success('Product saved successfully.');
-
         return redirect(route('products.index'));
     }
 
     public function show($id)
     {
         $product = $this->productRepository->find($id);
-
         if (empty($product)) {
             Flash::error('Product not found');
-
             return redirect(route('products.index'));
         }
-
         return view('products.show')->with('product', $product);
     }
 
     public function edit($id)
     {
         $product = $this->productRepository->find($id);
-
         if (empty($product)) {
             Flash::error('Product not found');
-
             return redirect(route('products.index'));
         }
-
         return view('products.edit')->with('product', $product);
     }
 
     public function update($id, UpdateproductRequest $request)
     {
         $product = $this->productRepository->find($id);
-
         if (empty($product)) {
             Flash::error('Product not found');
-
             return redirect(route('products.index'));
         }
-
         $product = $this->productRepository->update($request->all(), $id);
-
         Flash::success('Product updated successfully.');
-
         return redirect(route('products.index'));
     }
 
     public function destroy($id)
     {
         $product = $this->productRepository->find($id);
-
         if (empty($product)) {
             Flash::error('Product not found');
-
             return redirect(route('products.index'));
         }
-
         $this->productRepository->delete($id);
-
         Flash::success('Product deleted successfully.');
-
         return redirect(route('products.index'));
-    }
-
-    public function displayGrid(Request $request)
-    {
-        $products = \App\Models\Product::all();
-        return view('products.displaygrid')->with('products', $products);
     }
 }
